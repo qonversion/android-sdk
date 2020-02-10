6b465974f27f9f8703adf1a59d3f6037247bd913
artemy.glukhov <artemy.glukhov@corp.mail.ru>
2020-02-10 22:42:01 +0300

    Add usage examples of manual mode

---
+++ app/src/main/java/com/qonversion/android/app/ManualTrackingActivityKt.kt
@@ -0,0 +1,85 @@
+package com.qonversion.android.app
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.billingclient.api.*
+import com.qonversion.android.sdk.Qonversion
+import com.qonversion.android.sdk.Qonversion.Companion.instance
+import java.util.*
+
+class ManualTrackingActivityKt : AppCompatActivity() {
+    private var client: BillingClient? = null
+    private val skuDetails: MutableMap<String, SkuDetails> =
+        HashMap()
+
+    override fun onCreate(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onCreate(savedInstanceState, persistentState)
+        client = BillingClient
+            .newBuilder(this)
+            .enablePendingPurchases()
+            .setListener { billingResult, list ->
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    if (list != null && list.isNotEmpty()) {
+                        trackPurchase(
+                            skuDetails[SKU_ID]!!,
+                            list[0]
+                        )
+                    }
+                }
+            }
+            .build()
+        launchBilling()
+    }
+
+    private fun trackPurchase(
+        details: SkuDetails,
+        purchase: Purchase
+    ) {
+        Qonversion.instance!!.purchase(details, purchase)
+    }
+
+    private fun launchBilling() {
+        client!!.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    val params =
+                        SkuDetailsParams
+                            .newBuilder()
+                            .setSkusList(listOf(SKU_ID))
+                            .setType(BillingClient.SkuType.INAPP)
+                            .build()
+                    client!!.querySkuDetailsAsync(
+                        params
+                    ) { billingResult, list ->
+                        if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                            if (!list.isEmpty()) {
+                                skuDetails[SKU_ID] = list[0]
+                            }
+                            launchBillingFlow()
+                        }
+                    }
+                }
+            }
+
+            override fun onBillingServiceDisconnected() { // ignore in example
+            }
+        })
+    }
+
+    private fun launchBillingFlow() {
+        val params =
+            BillingFlowParams
+                .newBuilder()
+                .setSkuDetails(skuDetails[SKU_ID])
+                .build()
+        client!!.launchBillingFlow(this, params)
+    }
+
+    companion object {
+        private const val SKU_ID = "your_sku_id"
+    }
+}
