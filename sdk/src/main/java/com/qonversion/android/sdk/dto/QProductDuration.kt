abcefffb08be3571fc1033a1f48847e354b1dba8
Surik <surik.sarkisyan@gmail.com>
2020-11-11 17:03:01 +0300

    Removed a part of unused code

--- sdk/src/main/java/com/qonversion/android/sdk/dto/QProductDuration.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QProductDuration.kt
@@ -20,19 +20,3 @@ enum class QProductDuration(val type: Int) {
         }
     }
 }
-
-
-//enum class MyEnum(val type: Int) {
-//    A(1), B(2), C(3);
-//
-//    companion object {
-//        fun fromType(type: Int): MyEnum {
-//            return when (type) {
-//                1 -> A
-//                2 -> B
-//                3 -> C
-//                else -> throw IllegalArgumentException("Undefined enum type")
-//            }
-//        }
-//    }
-//}
