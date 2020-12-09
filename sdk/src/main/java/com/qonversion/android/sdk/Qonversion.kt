f70954c343195c4c72dd8c4286bc74e7a1446a4c
Maria-Bordunova <triphonovamaria@mail.ru>
2020-12-09 15:52:53 +0300

    Bump version to 2.1.1

--- sdk/src/main/java/com/qonversion/android/sdk/Qonversion.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/Qonversion.kt
@@ -13,7 +13,7 @@ import com.qonversion.android.sdk.validator.TokenValidator
 
 object Qonversion : LifecycleDelegate {
 
-    private const val SDK_VERSION = "2.1.0"
+    private const val SDK_VERSION = "2.1.1"
 
     private lateinit var repository: QonversionRepository
     private var userPropertiesManager: QUserPropertiesManager? = null
