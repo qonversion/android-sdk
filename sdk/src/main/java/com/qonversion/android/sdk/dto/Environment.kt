ec1beb118c720039fcba142bb6859c8bbe68842b
Maria-Bordunova <triphonovamaria@mail.ru>
2020-12-18 18:59:27 +0300

    Delete trackingEnabled and internalUserId fields

--- sdk/src/main/java/com/qonversion/android/sdk/dto/Environment.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/Environment.kt
@@ -2,8 +2,6 @@ package com.qonversion.android.sdk.dto
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import com.qonversion.android.sdk.dto.app.App
-import com.qonversion.android.sdk.dto.device.Device
 @JsonClass(generateAdapter = true)
 data class Environment(
     @Json(name = "app_version") val app_version: String,
@@ -17,6 +15,5 @@ data class Environment(
     @Json(name = "timezone") val timezone: String,
     @Json(name = "platform") val platform: String,
     @Json(name = "country") val country: String,
-    @Json(name = "tracking_enabled") val trackingEnabled: Int,
     @Json(name = "advertiser_id") val advertiserId: String?
 )
