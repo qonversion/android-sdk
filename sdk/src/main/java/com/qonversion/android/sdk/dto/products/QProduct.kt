62277b2fb756f4117841858985f1a8a7ba10cc37
Surik <surik.sarkisyan@gmail.com>
2021-01-20 19:32:42 +0800

    Added product trial period property

--- sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
@@ -15,9 +15,35 @@ data class QProduct(
     var skuDetail: SkuDetails? = null
         set(value) {
             prettyPrice = value?.price
+            trialDuration = configureTrialPeriod(value?.freeTrialPeriod)
             field = value
         }
 
     @Transient
     var prettyPrice: String? = null
+
+    @Transient
+    var trialDuration: QTrialDuration? = null
+
+    private fun configureTrialPeriod(trialPeriod: String?): QTrialDuration {
+        if (trialPeriod.isNullOrEmpty()) {
+            return QTrialDuration.NotAvailable
+        }
+
+        var period: QTrialDuration = QTrialDuration.Other
+
+        when (trialPeriod) {
+            "P3D"-> period = QTrialDuration.ThreeDays
+            "P7D", "P1W"-> period = QTrialDuration.Week
+            "P14D", "P2W"-> period = QTrialDuration.TwoWeeks
+            "P30D", "P1M"-> period = QTrialDuration.Month
+            "P60D", "P2M"-> period = QTrialDuration.TwoMonths
+            "P90D", "P3M"-> period = QTrialDuration.ThreeMonths
+            "P180D", "P6M"-> period = QTrialDuration.SixMonths
+            "P365D", "P12M", "P1Y"-> period = QTrialDuration.Year
+            else-> QTrialDuration.Other
+        }
+
+        return period
+    }
 }
