565343f4cbd4cd1ff661c9919959fa7641269087
Mary <triphonovamaria@mail.ru>
2021-02-26 12:09:45 +0300

    Get rid of compilation kapt warning

--- sdk/src/main/java/com/qonversion/android/sdk/storage/Cache.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/storage/Cache.kt
@@ -4,31 +4,64 @@ import com.squareup.moshi.JsonAdapter
 
 interface Cache {
 
-    fun putInt(key: String, value: Int)
     /**
-     * @param defValue is returned if the Int preference for key does not exist
+     * @param key Int preference key
+     * @param value Int preference value
+     */
+    fun putInt(key: String, value: Int)
+
+    /**
+     * @param key Int preference key
+     * @param defValue Value is returned if the Int preference for key does not exist
      */
     fun getInt(key: String, defValue: Int): Int
 
-    fun putFloat(key: String, value: Float)
     /**
-     * @param defValue is returned if the Float preference for key does not exist
+     * @param key Float preference key
+     * @param value Float preference value
+     */
+    fun putFloat(key: String, value: Float)
+
+    /**
+     * @param key for Float preference
+     * @param defValue Value is returned if the Float preference for key does not exist
      */
     fun getFloat(key: String, defValue: Float): Float
 
-    fun putLong(key: String, value: Long)
     /**
-     * @param defValue is returned if the Long preference for key does not exist
+     * @param key Long preference key
+     * @param value Long preference value
+     */
+    fun putLong(key: String, value: Long)
+
+    /**
+     * @param key for Long preference
+     * @param defValue Value is returned if the Long preference for key does not exist
      */
     fun getLong(key: String, defValue: Long): Long
 
-    fun putString(key: String, value: String?)
     /**
-    * @param defValue is returned if the String preference for key does not exist
-    */
+     * @param key String preference key
+     * @param value String preference value
+     */
+    fun putString(key: String, value: String?)
+
+    /**
+     * @param key String preference key
+     * @param defValue Value is returned if the String preference for key does not exist
+     */
     fun getString(key: String, defValue: String?): String?
 
+    /**
+     * @param key Object preference key
+     * @param value Object preference value
+     * @param adapter Adapter for storing an object in memory as a string
+     */
     fun <T> putObject(key: String, value: T, adapter: JsonAdapter<T>)
 
+    /**
+     * @param key for Object preference
+     * @param adapter Adapter for storing an object in memory as a string
+     */
     fun <T> getObject(key: String, adapter: JsonAdapter<T>): T?
 }
