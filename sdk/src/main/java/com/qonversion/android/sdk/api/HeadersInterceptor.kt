a19d286c174188a912067ad3b0bbaefff7b5a203
Maria Bordunova <triphonovamaria@mail.ru>
2021-03-11 22:50:50 +0300

    Add @Throws annotation

--- sdk/src/main/java/com/qonversion/android/sdk/api/HeadersInterceptor.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/api/HeadersInterceptor.kt
@@ -2,12 +2,14 @@ package com.qonversion.android.sdk.api
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 import javax.inject.Inject
 
 class HeadersInterceptor @Inject constructor(
     private val headersProvider: ApiHeadersProvider
 ) : Interceptor {
 
+    @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         request = request.newBuilder()
