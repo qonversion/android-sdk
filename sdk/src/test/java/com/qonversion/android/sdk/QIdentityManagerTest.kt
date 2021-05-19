a9a1a084701a51e5d7eadef9dd8e9c016115f4e8
Maria Bordunova <triphonovamaria@mail.ru>
2021-05-19 16:20:15 +0300

    Improve test logic

--- sdk/src/test/java/com/qonversion/android/sdk/QIdentityManagerTest.kt
+++ sdk/src/test/java/com/qonversion/android/sdk/QIdentityManagerTest.kt
@@ -1,16 +1,9 @@
 package com.qonversion.android.sdk
 
 import com.qonversion.android.sdk.services.QUserInfoService
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.assertj.core.api.Assertions.assertThat
-
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import org.junit.jupiter.api.*
 
 class QIdentityManagerTest {
     private val mockUserInfoService = mockk<QUserInfoService>(relaxed = true)
@@ -18,6 +11,7 @@ class QIdentityManagerTest {
 
     private val currentUserID = "currentUserID"
     private val newUserID = "newUserID"
+
     private lateinit var identityManager: QIdentityManager
 
     @BeforeEach
@@ -34,49 +28,22 @@ class QIdentityManagerTest {
     @Nested
     inner class Identify {
         @Test
-        fun `should call repository identify method with currentUserID only once`() {
-            // when
-            identityManager.identify(newUserID, object : IdentityManagerCallback {
-                override fun onSuccess(identityID: String) {}
-                override fun onError(error: QonversionError) {}
-            })
-
-            // then
-            verify(exactly = 1) {
-                mockRepository.identify(newUserID, currentUserID, any(), any())
-            }
-        }
-
-        @Test
         fun `should return non-empty identityID from onSuccess callback`() {
             // given
             val identityID = "identityID"
             var resultUserID: String? = null
-
-            every {
-                mockRepository.identify(newUserID, currentUserID, captureLambda(), any())
-            } answers {
-                lambda<(String) -> Unit>().captured.invoke(
-                    identityID
-                )
-            }
+            mockIdentifyResponse(identityID)
 
             // when
             identityManager.identify(newUserID, object : IdentityManagerCallback {
                 override fun onSuccess(identityID: String) {
                     resultUserID = identityID
                 }
-
-                override fun onError(error: QonversionError) {
-                    fail("Shouldn't go into onError callback")
-                }
+                override fun onError(error: QonversionError) {}
             })
 
             // then
             assertThat(resultUserID).isEqualTo(identityID)
-            verify(exactly = 1) {
-                mockUserInfoService.storeIdentity(resultUserID!!)
-            }
         }
 
         @Test
@@ -84,35 +51,61 @@ class QIdentityManagerTest {
             // given
             val identityID = ""
             var resultUserID: String? = null
-
-            every {
-                mockRepository.identify(newUserID, currentUserID, captureLambda(), any())
-            } answers {
-                lambda<(String) -> Unit>().captured.invoke(
-                    identityID
-                )
-            }
+            mockIdentifyResponse(identityID)
 
             // when
             identityManager.identify(newUserID, object : IdentityManagerCallback {
                 override fun onSuccess(identityID: String) {
                     resultUserID = identityID
                 }
-
-                override fun onError(error: QonversionError) {
-                    fail("Shouldn't go into onError callback")
-                }
+                override fun onError(error: QonversionError) {}
             })
 
             // then
             assertThat(resultUserID).isEqualTo(identityID)
-            verify(exactly = 0) {
-                mockUserInfoService.storeIdentity(any())
+        }
+
+        @Test
+        fun `should store non-empty identityID`() {
+            // given
+            val identityID = "identityID"
+            mockIdentifyResponse(identityID)
+
+            // when
+            identityManager.identify(newUserID, object : IdentityManagerCallback {
+                override fun onSuccess(identityID: String) {}
+                override fun onError(error: QonversionError) {}
+            })
+
+            // then
+            verifySequence {
+                mockUserInfoService.obtainUserID()
+                mockRepository.identify(newUserID, currentUserID, any(), any())
+                mockUserInfoService.storeIdentity(identityID)
             }
         }
 
         @Test
-        fun `should return qonversion error from onError callback`() {
+        fun `should not store empty identityID`() {
+            // given
+            val identityID = ""
+            mockIdentifyResponse(identityID)
+
+            // when
+            identityManager.identify(newUserID, object : IdentityManagerCallback {
+                override fun onSuccess(identityID: String) {}
+                override fun onError(error: QonversionError) {}
+            })
+
+            // then
+            verifySequence {
+                mockUserInfoService.obtainUserID()
+                mockRepository.identify(newUserID, currentUserID, any(), any())
+            }
+        }
+
+        @Test
+        fun `should return error from onError callback`() {
             // given
             var qError: QonversionError? = null
 
@@ -126,10 +119,7 @@ class QIdentityManagerTest {
 
             // when
             identityManager.identify(newUserID, object : IdentityManagerCallback {
-                override fun onSuccess(identityID: String) {
-                    fail("Shouldn't go into onSuccess callback")
-                }
-
+                override fun onSuccess(identityID: String) {}
                 override fun onError(error: QonversionError) {
                     qError = error
                 }
@@ -139,6 +129,16 @@ class QIdentityManagerTest {
             assertThat(qError).isNotNull
             assertThat(qError!!.code)
                 .isEqualTo(QonversionErrorCode.BackendError)
+        }
+
+        private fun mockIdentifyResponse(identityID: String) {
+            every {
+                mockRepository.identify(newUserID, currentUserID, captureLambda(), any())
+            } answers {
+                lambda<(String) -> Unit>().captured.invoke(
+                    identityID
+                )
+            }
         }
     }
 
--- sdk/src/test/java/com/qonversion/android/sdk/services/QUserInfoServiceTest.kt
+++ sdk/src/test/java/com/qonversion/android/sdk/services/QUserInfoServiceTest.kt
@@ -15,6 +15,8 @@ class QUserInfoServiceTest {
 
     private val prefsOriginalUserIdKey = "com.qonversion.keys.originalUserID"
     private val prefsUserIdKey = "com.qonversion.keys.storedUserID"
+    private val randomUID = "08111735c1a641f085cae9d0ab98a642"
+    private val generatedUID = "QON_$randomUID"
 
     private lateinit var userInfoService: QUserInfoService
 
@@ -28,143 +30,290 @@ class QUserInfoServiceTest {
     @Nested
     inner class ObtainUserID {
         @Test
-        fun `should load UID from storage when preferences is empty`() {
+        fun `should load UID from token storage when UID from preferences is empty`() {
             // given
             val userID = ""
-            val token = "token"
-
-            every {
-                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
-            } returns userID
-
-            every {
-                mockTokenStorage.load()
-            } returns token
+            mockUserIDCache(userID)
 
             // when
-            val resultId = userInfoService.obtainUserID()
+            userInfoService.obtainUserID()
 
             // then
-            verify(exactly = 1) {
+            verifySequence {
                 mockTokenStorage.load()
                 mockTokenStorage.delete()
-                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, token)
-                mockSharedPreferencesCache.putString(prefsUserIdKey, token)
             }
-
-            Assertions.assertThat(resultId).isEqualTo(token)
         }
 
         @Test
-        fun `should generate UID when preferences and storage are empty`() {
+        fun `should load and delete UID from token storage when UID from preferences is null`() {
             // given
-            val userID = ""
-            val token = ""
-            val randomUID = "08111735c1a641f085cae9d0ab98a642"
-            val generatedUID = "QON_$randomUID"
-
-            every {
-                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
-            } returns userID
-
-            every {
-                mockTokenStorage.load()
-            } returns token
-
-            mockkStatic(UUID::class)
-            every {
-                UUID.randomUUID().toString()
-            } returns randomUID
+            val userID = null
+            mockUserIDCache(userID)
 
             // when
-            val resultId = userInfoService.obtainUserID()
+            userInfoService.obtainUserID()
 
             // then
-            verify(exactly = 1) {
+            verifySequence {
                 mockTokenStorage.load()
                 mockTokenStorage.delete()
-                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, generatedUID)
-                mockSharedPreferencesCache.putString(prefsUserIdKey, generatedUID)
             }
-
-            Assertions.assertThat(resultId).isEqualTo(generatedUID)
         }
 
         @Test
-        fun `should load UID from preferences when it is not empty`() {
+        fun `should not load UID from token storage when UID from preferences is not empty`() {
             // given
             val userID = "userID"
-
-            every {
-                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
-            } returns userID
+            mockUserIDCache(userID)
 
             // when
-            val resultId = userInfoService.obtainUserID()
+            userInfoService.obtainUserID()
 
             // then
             verify(exactly = 0) {
                 mockTokenStorage.load()
-                mockTokenStorage.delete()
-                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, any())
-                mockSharedPreferencesCache.putString(prefsUserIdKey, any())
             }
-
-            Assertions.assertThat(resultId).isEqualTo(userID)
         }
-    }
 
-    @Nested
-    inner class StoreIdentity {
         @Test
-        fun `should store userID to preferences`() {
+        fun `should not delete UID from token storage when UID from preferences is not empty`() {
             // given
             val userID = "userID"
+            mockUserIDCache(userID)
 
             // when
-            userInfoService.storeIdentity(userID)
+            userInfoService.obtainUserID()
 
             // then
-            verify(exactly = 1) {
-                mockSharedPreferencesCache.putString(prefsUserIdKey, userID)
+            verify(exactly = 0) {
+                mockTokenStorage.delete()
             }
+        }
+
+        @Test
+        fun `should generate random UID when UID from token storage is empty`() {
+            // given
+            val userID = ""
+            val token = ""
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+            mockUUID()
+
+            // when
+            val resultID = userInfoService.obtainUserID()
+
+            // then
+            Assertions.assertThat(resultID).isEqualTo(generatedUID)
+        }
+
+        @Test
+        fun `should not generate random UID when UID from token storage is empty`() {
+            // given
+            val userID = ""
+            val token = "token"
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+
+            // when
+            val resultId = userInfoService.obtainUserID()
+
+            // then
+            Assertions.assertThat(resultId).isEqualTo(token)
+        }
+
+        @Test
+        fun `should save generated UID to preferences when UID from preferences is empty`() {
+            // given
+            val userID = ""
+            val token = ""
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+            mockUUID()
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verifySequence {
+                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
+                mockSharedPreferencesCache.putString(prefsUserIdKey, generatedUID)
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, generatedUID)
+            }
+        }
+
+        @Test
+        fun `should save generated UID to preferences when UID from preferences is null`() {
+            // given
+            val userID = null
+            val token = ""
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+            mockUUID()
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verifySequence {
+                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
+                mockSharedPreferencesCache.putString(prefsUserIdKey, generatedUID)
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, generatedUID)
+            }
+        }
+
+        @Test
+        fun `should save UID from token storage to preferences when UID from preferences is empty`() {
+            // given
+            val userID = ""
+            val token = "token"
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verifySequence {
+                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
+                mockSharedPreferencesCache.putString(prefsUserIdKey, token)
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, token)
+            }
+        }
+
+        @Test
+        fun `should save UID from token storage to preferences when UID from preferences is null`() {
+            // given
+            val userID = null
+            val token = "token"
+            mockUserIDCache(userID)
+            mockTokenStorage(token)
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verifySequence {
+                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
+                mockSharedPreferencesCache.putString(prefsUserIdKey, token)
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, token)
+            }
+        }
+
+        @Test
+        fun `should not save UID as storedUserID when UID from preferences is not empty`() {
+            // given
+            val userID = "userID"
+            mockUserIDCache(userID)
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verify(exactly = 0) {
+                mockSharedPreferencesCache.putString(prefsUserIdKey, any())
+            }
+        }
+
+        @Test
+        fun `should not save UID as originalUserID when UID from preferences is not empty`() {
+            // given
+            val userID = "userID"
+            mockUserIDCache(userID)
+
+            // when
+            userInfoService.obtainUserID()
+
+            // then
+            verify(exactly = 0) {
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, any())
+            }
+        }
+
+        private fun mockUserIDCache(userID: String?) {
+            every {
+                mockSharedPreferencesCache.getString(prefsUserIdKey, null)
+            } returns userID
+        }
+
+        private fun mockTokenStorage(token: String) {
+            every {
+                mockTokenStorage.load()
+            } returns token
+        }
+
+        private fun mockUUID() {
+            mockkStatic(UUID::class)
+            every {
+                UUID.randomUUID().toString()
+            } returns randomUID
         }
     }
 
-    @Nested
-    inner class Logout {
-        @Test
-        fun `should store original userID to preferences`() {
-            // given
-            val originalUserID = "originalUserID"
+    @Test
+    fun storeIdentity() {
+        // given
+        val userID = "userID"
 
-            every {
-                mockSharedPreferencesCache.getString(prefsOriginalUserIdKey, null)
-            } returns originalUserID
+        // when
+        userInfoService.storeIdentity(userID)
 
-            // when
-            userInfoService.logout()
+        // then
+        verify(exactly = 1) {
+            mockSharedPreferencesCache.putString(prefsUserIdKey, userID)
+        }
+    }
 
-            // then
-            verify(exactly = 1) {
-                mockSharedPreferencesCache.putString(prefsUserIdKey, originalUserID)
-            }
+    @Test
+    fun logout() {
+        // given
+        val originalUserID = "originalUserID"
+
+        every {
+            mockSharedPreferencesCache.getString(prefsOriginalUserIdKey, null)
+        } returns originalUserID
+
+        // when
+        userInfoService.logout()
+
+        // then
+        verifySequence {
+            mockSharedPreferencesCache.getString(prefsOriginalUserIdKey, null)
+            mockSharedPreferencesCache.putString(prefsUserIdKey, originalUserID)
         }
     }
 
     @Nested
     inner class DeleteUser {
         @Test
-        fun `should empty preferences and storage`() {
-            // given
+        fun `should empty preferences from originalUserID`() {
+            // when
+            userInfoService.deleteUser()
 
+            // then
+            verify(exactly = 1) {
+                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, null)
+            }
+        }
+
+        @Test
+        fun `should empty preferences from storedUserID`() {
             // when
             userInfoService.deleteUser()
 
             // then
             verify(exactly = 1) {
                 mockSharedPreferencesCache.putString(prefsUserIdKey, null)
-                mockSharedPreferencesCache.putString(prefsOriginalUserIdKey, null)
+            }
+        }
+
+        @Test
+        fun `should empty token storage`() {
+            // when
+            userInfoService.deleteUser()
+
+            // then
+            verify(exactly = 1) {
                 mockTokenStorage.delete()
             }
         }
