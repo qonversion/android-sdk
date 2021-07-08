7a06ff1443fec8d28245e8df364cf20c614edd3e
Maria Bordunova <triphonovamaria@mail.ru>
2021-07-08 15:33:23 +0300

    Delete sku from UpdatePurchaseInfo

--- sdk/src/main/java/com/qonversion/android/sdk/billing/UpdatePurchaseInfo.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/billing/UpdatePurchaseInfo.kt
@@ -3,7 +3,6 @@ package com.qonversion.android.sdk.billing
 import com.android.billingclient.api.BillingFlowParams
 
 data class UpdatePurchaseInfo(
-    val oldSku: String,
     val purchaseToken: String,
     @BillingFlowParams.ProrationMode val prorationMode: Int? = null
 )
