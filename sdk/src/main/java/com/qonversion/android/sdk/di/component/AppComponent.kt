4cb666c7c5ecb94af2cfba40fb16b48906a6dd4a
SCpRoION <spertsiankamo@gmail.com>
2021-11-17 16:06:54 +0300

    Detekt fixes.

--- sdk/src/main/java/com/qonversion/android/sdk/di/component/AppComponent.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/di/component/AppComponent.kt
@@ -8,14 +8,24 @@ import com.qonversion.android.sdk.QonversionConfig
 import com.qonversion.android.sdk.QonversionRepository
 import com.qonversion.android.sdk.di.scope.ApplicationScope
 import com.qonversion.android.sdk.automations.QAutomationsManager
-import com.qonversion.android.sdk.di.module.*
+import com.qonversion.android.sdk.di.module.AppModule
+import com.qonversion.android.sdk.di.module.RepositoryModule
+import com.qonversion.android.sdk.di.module.NetworkModule
+import com.qonversion.android.sdk.di.module.ManagersModule
+import com.qonversion.android.sdk.di.module.ServicesModule
 import com.qonversion.android.sdk.services.QUserInfoService
 import com.qonversion.android.sdk.storage.LaunchResultCacheWrapper
 import com.qonversion.android.sdk.storage.PurchasesCache
 import dagger.Component
 
 @ApplicationScope
-@Component(modules = [AppModule::class, RepositoryModule::class, NetworkModule::class, ManagersModule::class, ServicesModule::class])
+@Component(modules = [
+    AppModule::class,
+    RepositoryModule::class,
+    NetworkModule::class,
+    ManagersModule::class,
+    ServicesModule::class
+])
 interface AppComponent {
     fun repository(): QonversionRepository
     fun purchasesCache(): PurchasesCache
