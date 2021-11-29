454e5ccc6c66950a54831d406941592092767c6c
kamospertsyan <spertsiankamo@gmail.com>
2021-11-29 16:55:50 +0300

    ApiV2RequestConfigurator with users requests.

---
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/ApiV2RequestConfigurator.kt
@@ -0,0 +1,24 @@
+package com.qonversion.android.sdk.internal.networkLayer.requestConfigurator
+
+import com.qonversion.android.sdk.internal.networkLayer.dto.Request
+import com.qonversion.android.sdk.internal.networkLayer.headerBuilder.HeaderBuilder
+
+class ApiV2RequestConfigurator(
+    private val headerBuilder: HeaderBuilder
+): RequestConfigurator {
+
+    override fun configureUserRequest(id: String): Request {
+        val headers = headerBuilder.buildCommonHeaders()
+        return Request.get("$BASE_URL/users/$id", headers)
+    }
+
+    override fun configureCreateUserRequest(id: String): Request {
+        val headers = headerBuilder.buildCommonHeaders()
+        val body = mapOf("id" to id)
+        return Request.post("$BASE_URL/users", headers, body)
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.qonversion.io/v2"
+    }
+}
--- sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfigurator.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfigurator.kt
@@ -1,3 +1,10 @@
 package com.qonversion.android.sdk.internal.networkLayer.requestConfigurator
 
-interface RequestConfigurator
+import com.qonversion.android.sdk.internal.networkLayer.dto.Request
+
+interface RequestConfigurator {
+
+    fun configureUserRequest(id: String): Request
+
+    fun configureCreateUserRequest(id: String): Request
+}
