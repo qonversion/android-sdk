566aa3ae2095676b631a44c47c2f940366f66d30
Surik <surik.sarkisyan@gmail.com>
2021-11-30 12:44:11 +0300

    Added few fixes

--- sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/networkClient/NetworkClientDecoratorImpl.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/networkClient/NetworkClientDecoratorImpl.kt
@@ -1,58 +1,63 @@
 package com.qonversion.android.sdk.internal.networkLayer.networkClient
 
 import com.qonversion.android.sdk.internal.InternalConfig
-import com.qonversion.android.sdk.internal.mappers.Mapper
+import com.qonversion.android.sdk.internal.exception.ErrorCode
+import com.qonversion.android.sdk.internal.exception.QonversionException
 import com.qonversion.android.sdk.internal.networkLayer.RetryPolicy
 import com.qonversion.android.sdk.internal.networkLayer.dto.Request
 import com.qonversion.android.sdk.internal.networkLayer.dto.Response
 import com.qonversion.android.sdk.internal.networkLayer.retryDelayCalculator.RetryDelayCalculator
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 data class RetryConfig(
     internal val shouldRetry: Boolean,
     internal val attemptIndex: Int = 0,
-    internal val delay: Long = 0L)
+    internal val delay: Long = 0L
+)
 
 class NetworkClientDecoratorImpl internal constructor(
     private val networkClient: NetworkClient,
     private val defaultRetryPolicy: RetryPolicy = RetryPolicy.Exponential(),
     private val delayCalculator: RetryDelayCalculator,
     private val config: InternalConfig
-): NetworkClientDecorator {
-    override suspend fun execute(request: Request, retryPolicy: RetryPolicy): Response {
-        return execute(request, retryPolicy, 0)
-    }
-
+) : NetworkClientDecorator {
     override suspend fun execute(request: Request): Response {
         return execute(request, defaultRetryPolicy)
     }
 
+    override suspend fun execute(request: Request, retryPolicy: RetryPolicy): Response {
+        return execute(request, retryPolicy, 0)
+    }
+
     private suspend fun execute(request: Request, retryPolicy: RetryPolicy, attemptIndex: Int): Response {
-        if (config.requestsShouldBeDenied) {
-            // return default error
-            return Response(200, mapOf<String, Any>())
-        }
-        val response = networkClient.execute(request)
-        return if (response.isSuccess) {
-            response
-        } else {
-            val retryConfig: RetryConfig = prepareRetryConfig(retryPolicy, attemptIndex)
-            if (retryConfig.shouldRetry) {
-                delay(retryConfig.delay)
-                execute(request, retryPolicy, retryConfig.attemptIndex)
-            } else {
+        return withContext(Dispatchers.IO) {
+            if (config.requestsShouldBeDenied) {
+                throw QonversionException(ErrorCode.RequestDenied)
+            }
+            val response = networkClient.execute(request)
+            if (response.isSuccess) {
                 response
+            } else {
+                val retryConfig: RetryConfig = prepareRetryConfig(retryPolicy, attemptIndex)
+                if (retryConfig.shouldRetry) {
+                    delay(retryConfig.delay)
+                    execute(request, retryPolicy, retryConfig.attemptIndex)
+                } else {
+                    response
+                }
             }
         }
     }
 
     private fun prepareRetryConfig(retryPolicy: RetryPolicy, attemptIndex: Int): RetryConfig {
         var shouldRetry = false
-        val newAttemptIndex: Int = attemptIndex + 1
+        val newAttemptIndex = attemptIndex + 1
         var minDelay = 0L
         var delay = 0L
 
-        if (retryPolicy is RetryPolicy.InfinityExponential) {
+        if (retryPolicy is RetryPolicy.InfiniteExponential) {
             shouldRetry = true
             minDelay = retryPolicy.minDelay
         } else if (retryPolicy is RetryPolicy.Exponential) {
