6bdded87b81d26ee59b039a08258b4c65c1aa52b
kamospertsyan <spertsiankamo@gmail.com>
2021-12-01 16:45:05 +0300

    CR fixes

--- sdk/src/main/java/com/qonversion/android/sdk/internal/common/InternalConstants.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/common/InternalConstants.kt
@@ -1,7 +1,13 @@
 package com.qonversion.android.sdk.internal.common
 
+internal const val BASE_API_URL = "https://api.qonversion.io"
+internal const val API_VERSION = "v2"
+internal const val API_URL = "$BASE_API_URL/$API_VERSION"
+
 internal const val PLATFORM = "android"
+
 internal const val DEBUG_MODE_PREFIX = "test_"
+
 internal const val PREFS_PREFIX = "com.qonversion.keys"
 internal const val PREFS_SOURCE_KEY = "$PREFS_PREFIX.source"
 internal const val PREFS_SOURCE_VERSION_KEY = "$PREFS_PREFIX.sourceVersion"
--- sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfiguratorImpl.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfiguratorImpl.kt
@@ -3,22 +3,21 @@ package com.qonversion.android.sdk.internal.networkLayer.requestConfigurator
 import com.qonversion.android.sdk.internal.networkLayer.dto.Request
 import com.qonversion.android.sdk.internal.networkLayer.headerBuilder.HeaderBuilder
 
-internal class ApiV2RequestConfigurator(
-    private val headerBuilder: HeaderBuilder
+internal class RequestConfiguratorImpl(
+    private val headerBuilder: HeaderBuilder,
+    private val baseUrl: String
 ): RequestConfigurator {
 
     override fun configureUserRequest(id: String): Request {
         val headers = headerBuilder.buildCommonHeaders()
-        return Request.get("$BASE_URL/users/$id", headers)
+
+        return Request.get("$baseUrl/${ApiEndpoint.Users.path}/$id", headers)
     }
 
     override fun configureCreateUserRequest(id: String): Request {
         val headers = headerBuilder.buildCommonHeaders()
         val body = mapOf("id" to id)
-        return Request.post("$BASE_URL/users", headers, body)
-    }
 
-    companion object {
-        const val BASE_URL = "https://api.qonversion.io/v2"
+        return Request.post("$baseUrl/${ApiEndpoint.Users.path}", headers, body)
     }
 }
