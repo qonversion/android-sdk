8a17e6c190cff729ba16655e0e5a14902d42b49c
Kamo Spertsyan <spertsiankamo@gmail.com>
2021-12-01 16:55:41 +0300

    Request configurator implementation. (#257)
    * Added a part of mapping logic
    
    * Added a part of headers builder logic
    
    * ApiV2RequestConfigurator with users requests.
    
    * Remove old files.
    
    * CR fixes
    
    * Detekt detekted.
    
    Co-authored-by: Surik <surik.sarkisyan@gmail.com>

---
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/ApiEndpoint.kt
@@ -0,0 +1,5 @@
+package com.qonversion.android.sdk.internal.networkLayer.requestConfigurator
+
+enum class ApiEndpoint(val path: String) {
+    Users("users")
+}
--- sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfigurator.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/networkLayer/requestConfigurator/RequestConfigurator.kt
@@ -1,3 +1,10 @@
 package com.qonversion.android.sdk.internal.networkLayer.requestConfigurator
 
-internal interface RequestConfigurator
+import com.qonversion.android.sdk.internal.networkLayer.dto.Request
+
+internal interface RequestConfigurator {
+
+    fun configureUserRequest(id: String): Request
+
+    fun configureCreateUserRequest(id: String): Request
+}
