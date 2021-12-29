5a6cb43ce21f4c778d7015788c0c7cf9e913e90b
kamospertsyan <spertsiankamo@gmail.com>
2021-12-29 13:00:54 +0300

    CachedObject - data class.

--- sdk/src/main/java/com/qonversion/android/sdk/internal/cache/CachedObject.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/cache/CachedObject.kt
@@ -2,4 +2,4 @@ package com.qonversion.android.sdk.internal.cache
 
 import java.util.Date
 
-internal class CachedObject<T>(val date: Date, var value: T?)
+internal data class CachedObject<T>(val date: Date, var value: T?)
