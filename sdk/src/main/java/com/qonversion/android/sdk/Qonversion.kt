7374b3dc88342ead739c37366ef0d85580d1b2f5
Surik <surik.sarkisyan@gmail.com>
2022-01-20 17:37:46 +0300

    Added code docs

--- sdk/src/main/java/com/qonversion/android/sdk/Qonversion.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/Qonversion.kt
@@ -13,10 +13,26 @@ interface Qonversion {
 
         private var backingInstance: Qonversion? = null
 
+        /**
+         * Use this variable to get the current initialized instance of Qonversion SDK.
+         * Please, use this variable strict after call Qonversion.initialize().
+         * Otherwise, this variable throws an exception.
+         * @return the current initialized instance of Qonversion SDK.
+         * @throws QonversionException with [ErrorCode.NotInitialized]
+         */
         @JvmStatic
         val sharedInstance: Qonversion
             get() = backingInstance ?: throw QonversionException(ErrorCode.NotInitialized)
 
+        /**
+         * This function is an entry point to use Qonversion SDK.
+         * Call to initialize Qonversion SDK with required and additional configs.
+         * This function is the best way to set all additional configs you need to use Qonversion SDK.
+         * You still have an option to set a part of additional configs later
+         * via calling separated setters.
+         * @param config - a config you should get using [QonversionConfig.Builder.build]
+         * @return the initialized instance of Qonversion SDK.
+         */
         @JvmStatic
         fun initialize(config: QonversionConfig): Qonversion {
             return QonversionInternal(config).also {
@@ -25,13 +41,44 @@ interface Qonversion {
         }
     }
 
+    /**
+     * Call to set an [Environment].
+     * You may call this function to set an environment separately from [Qonversion.initialize]
+     * Call this function only in case you are sure you need to set an environment after [Qonversion.initialize].
+     * Otherwise, set an environment via [Qonversion.initialize].
+     * @param environment - an environment.
+     */
     fun setEnvironment(environment: Environment)
 
+    /**
+     * Use this function to change the level of logs that Qonversion SDK will print.
+     * See [LogLevel] for details.
+     * @param logLevel - a preferred log level.
+     */
     fun setLogLevel(logLevel: LogLevel)
 
+    /**
+     * Use this function to set the log tag that Qonversion SDK will print with every log message.
+     * You can use it, for example, to filter Qonversion SDK logs and your App own logs together.
+     * @param logTag - a preferred log tag.
+     */
     fun setLogTag(logTag: String)
 
+    /**
+     * Use this function to set the cache lifetime.
+     * Qonversion SDK uses this value in case when user's internet connection is not stable.
+     * You can use it, for example, if your App could be used without an internet connection.
+     * Set the cache lifetime as long as possible,
+     * so you will not affect UX for users using your App offline.
+     * @param cacheLifetime: - a preferred cache life time.
+     */
     fun setCacheLifetime(cacheLifetime: CacheLifetime)
 
+    /**
+     * Call this function when you are done with the current instance of Qonversion SDK.
+     * Please, make sure you have a reason to finish the current instance and initialize the new one.
+     * Initializing a new (not the first) instance of QonversionSDK is not necessary
+     * for the most part of use cases.
+     */
     fun finish()
 }
--- sdk/src/main/java/com/qonversion/android/sdk/dto/LogLevel.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/LogLevel.kt
@@ -1,9 +1,28 @@
 package com.qonversion.android.sdk.dto
 
+import com.qonversion.android.sdk.Qonversion
+
+/**
+ * This enum contains an available settings for LogLevel.
+ * Provide it to the configuration object via [Qonversion.initialize] while initializing the SDK
+ * or via [Qonversion.setLogLevel] after initializing the SDK.
+ * The default value is [LogLevel.Info].
+ *
+ * You could change the log level to make logs from Qonversion SDK more detailed or strict.
+ */
 enum class LogLevel(val level: Int) {
+    // all available logs (function started, function finished, data fetched, etc)
     Verbose(0),
+
+    // info level (data fetched, products loaded, user info fetched, etc)
     Info(10),
+
+    // warning level (data fetched partially, sandbox env enabled for release build, etc)
     Warning(20),
+
+    // error level (data fetch failed, Google billing is not available, etc)
     Error(30),
+
+    // logging disabled at all
     Disabled(Int.MAX_VALUE)
 }
