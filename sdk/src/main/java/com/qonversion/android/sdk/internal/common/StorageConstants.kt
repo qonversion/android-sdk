fe75418c65026857c83105d2e1ad8328baeb326d
Maria-Bordunova <triphonovamaria@mail.ru>
2022-01-28 18:25:57 +0300

    Fix detekt

--- sdk/src/main/java/com/qonversion/android/sdk/internal/common/StorageConstants.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/common/StorageConstants.kt
@@ -5,8 +5,8 @@ private const val PREFS_PREFIX = "com.qonversion.keys"
 internal enum class StorageConstants(val key: String) {
     SourceKey("$PREFS_PREFIX.source"),
     VersionKey("$PREFS_PREFIX.sourceVersion"),
-    UserId("${PREFS_PREFIX}.storedUserID"),
-    OriginalUserId("${PREFS_PREFIX}.originalUserID"),
+    UserId("$PREFS_PREFIX.storedUserID"),
+    OriginalUserId("$PREFS_PREFIX.originalUserID"),
     Token("token_key"), // Deprecated location of user id - was used in old sdk versions.
     UserProperties("$PREFS_PREFIX.userProperties")
 }
