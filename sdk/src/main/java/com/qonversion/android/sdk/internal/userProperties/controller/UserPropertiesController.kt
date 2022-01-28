47c0e26126883bfb4af575ccd645c0dfc7e91b0c
kamospertsyan <spertsiankamo@gmail.com>
2022-01-28 18:09:46 +0300

    Remove `throws` tags from interface.

--- sdk/src/main/java/com/qonversion/android/sdk/internal/userProperties/controller/UserPropertiesController.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/userProperties/controller/UserPropertiesController.kt
@@ -1,8 +1,5 @@
 package com.qonversion.android.sdk.internal.userProperties.controller
 
-import com.qonversion.android.sdk.internal.exception.QonversionException
-import kotlin.jvm.Throws
-
 internal interface UserPropertiesController {
 
     fun setProperty(key: String, value: String)
