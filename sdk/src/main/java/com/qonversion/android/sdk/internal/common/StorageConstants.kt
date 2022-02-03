6713236dbff74cb9dbbf0f8553508f8226c69e64
Surik Sarkisyan <suriksarkisyan@users.noreply.github.com>
2022-02-03 15:41:39 +0300

    Added logic to avoid sending properties that already sent (#301)
    * Added logic to avoid sending properties that already sent

--- sdk/src/main/java/com/qonversion/android/sdk/internal/common/StorageConstants.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/common/StorageConstants.kt
@@ -1,6 +1,7 @@
 package com.qonversion.android.sdk.internal.common
 
 private const val PREFS_PREFIX = "com.qonversion.keys"
+private const val USER_PROPERTIES_PREFIX = "userProperties"
 
 internal enum class StorageConstants(val key: String) {
     SourceKey("$PREFS_PREFIX.source"),
@@ -8,5 +9,6 @@ internal enum class StorageConstants(val key: String) {
     UserId("$PREFS_PREFIX.storedUserID"),
     OriginalUserId("$PREFS_PREFIX.originalUserID"),
     Token("token_key"), // Deprecated location of user id - was used in old sdk versions.
-    UserProperties("$PREFS_PREFIX.userProperties")
+    PendingUserProperties("$PREFS_PREFIX.$USER_PROPERTIES_PREFIX.pending"),
+    SentUserProperties("$PREFS_PREFIX.$USER_PROPERTIES_PREFIX.sent")
 }
