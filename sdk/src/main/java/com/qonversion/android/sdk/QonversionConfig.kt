813b087811da73bf8f4011a24953873b783c1be2
Kamo Spertsyan <spertsiankamo@gmail.com>
2022-02-09 13:25:19 +0300

    Added QonversionConfig documentation. (#303)
    * Added QonversionConfig documentation.
    
    * PR fixes.
    
    * Update sdk/src/main/java/com/qonversion/android/sdk/QonversionConfig.kt
    
    Co-authored-by: Maria Bordunova <39134139+Maria-Bordunova@users.noreply.github.com>
    
    * Update sdk/src/main/java/com/qonversion/android/sdk/QonversionConfig.kt
    
    Co-authored-by: Maria Bordunova <39134139+Maria-Bordunova@users.noreply.github.com>
    
    * Documentation fixes.
    
    Co-authored-by: Maria Bordunova <39134139+Maria-Bordunova@users.noreply.github.com>

--- sdk/src/main/java/com/qonversion/android/sdk/QonversionConfig.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/QonversionConfig.kt
@@ -17,15 +17,36 @@ import com.qonversion.android.sdk.internal.utils.isDebuggable
 
 private const val DEFAULT_LOG_TAG = "Qonversion"
 
+/**
+ * This class contains all the available configurations for the initialization of Qonversion SDK.
+ *
+ * To create an instance, use the nested [Builder] class.
+ *
+ * You should pass the created instance to the [Qonversion.initialize] method.
+ *
+ * @see [The documentation](https://documentation.qonversion.io/v3.0/docs/configuring-the-sdks)
+ */
 class QonversionConfig internal constructor(
-    val application: Application,
-    val primaryConfig: PrimaryConfig,
-    val storeConfig: StoreConfig,
-    val loggerConfig: LoggerConfig,
-    val networkConfig: NetworkConfig,
-    val cacheLifetime: CacheLifetime
+    internal val application: Application,
+    internal val primaryConfig: PrimaryConfig,
+    internal val storeConfig: StoreConfig,
+    internal val loggerConfig: LoggerConfig,
+    internal val networkConfig: NetworkConfig,
+    internal val cacheLifetime: CacheLifetime
 ) {
 
+    /**
+     * The builder of Qonversion configuration instance.
+     *
+     * This class contains a variety of methods to customize the Qonversion SDK behavior.
+     * You can call them sequentially and call [build] finally to get the configuration instance.
+     *
+     * @constructor creates an instance of a builder
+     * @param application the instance of the current running application
+     * @param projectKey your Project Key from Qonversion Dashboard
+     * @param launchMode launch mode of the Qonversion SDK todo add link
+     * @param store the store used for purchases (only [Store.GooglePlay] is supported for now)
+     */
     class Builder @JvmOverloads constructor(
         private val application: Application,
         private val projectKey: String,
@@ -38,26 +59,79 @@ class QonversionConfig internal constructor(
         internal var cacheLifetime = CacheLifetime.ThreeDays
         internal var shouldConsumePurchases = true
 
+        /**
+         * Set current application [Environment]. Used to distinguish sandbox and production users.
+         *
+         * @param environment current environment.
+         * @return builder instance for chain calls.
+         */
         fun setEnvironment(environment: Environment): Builder = apply {
             this.environment = environment
         }
 
+        /**
+         * Define the maximum lifetime of the data cached by Qonversion.
+         * It means that cached data won't be used if it is older than the provided duration.
+         * By the way it doesn't mean that cache will live exactly the provided time.
+         * It may be updated earlier.
+         *
+         * Provide as bigger value as possible for you taking into account, among other things,
+         * how long may your users remain without the internet connection and so on.
+         *
+         * @param cacheLifetime the desired lifetime of Qonversion caches.
+         * @return builder instance for chain calls.
+         */
         fun setCacheLifetime(cacheLifetime: CacheLifetime): Builder = apply {
             this.cacheLifetime = cacheLifetime
         }
 
+        /**
+         * Define the level of the logs that the SDK prints.
+         * The more strict the level is, the less logs will be written.
+         * For example, setting the log level as Warning will disable all info and verbose logs.
+         *
+         * @param logLevel the desired allowed log level.
+         * @return builder instance for chain calls.
+         */
         fun setLogLevel(logLevel: LogLevel): Builder = apply {
             this.logLevel = logLevel
         }
 
+        /**
+         * Define the log tag that the Qonversion SDK will print with every log message.
+         * For example, you can use it to filter the Qonversion SDK logs and your app own logs together.
+         *
+         * @param logTag the desired log tag.
+         * @return builder instance for chain calls.
+         */
         fun setLogTag(logTag: String): Builder = apply {
             this.logTag = logTag
         }
 
+        /**
+         * Define should Qonversion consume purchases itself or not.
+         * You may need to consume purchases yourself if you want to add custom handling of them
+         * before it, for example, send the purchase to API or hand over coins to the user.
+         * If set to false make sure that you call [Qonversion.consume] for purchases yourself.
+         * todo fix method link above when it will be implemented
+         *
+         * The flag makes sense only for [LaunchMode.InfrastructureMode].
+         *
+         * @param shouldConsumePurchases if true, the Qonversion will consume purchases itself. If false - it won't.
+         * @return builder instance for chain calls.
+         */
         fun setShouldConsumePurchases(shouldConsumePurchases: Boolean): Builder = apply {
             this.shouldConsumePurchases = shouldConsumePurchases
         }
 
+        /**
+         * Generate [QonversionConfig] instance with all the provided configurations.
+         * This method also validates some of the provided data.
+         *
+         * @throws QonversionException if unacceptable configuration was provided.
+         * @return the complete [QonversionConfig] instance.
+         */
+        @Throws(QonversionException::class)
         fun build(): QonversionConfig {
             if (projectKey.isBlank()) {
                 throw QonversionException(ErrorCode.ConfigPreparation, "Project key is empty")
