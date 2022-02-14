cf943399c059c9872bf01597412c70ef10a412a8
kamospertsyan <spertsiankamo@gmail.com>
2022-02-14 12:52:46 +0300

    UserIdGenerator implementation.

---
+++ sdk/src/test/java/com/qonversion/android/sdk/internal/user/generator/UserIdGeneratorTest.kt
@@ -0,0 +1,26 @@
+package com.qonversion.android.sdk.internal.user.generator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class UserIdGeneratorTest {
+
+    private val generator: UserIdGenerator = UserIdGeneratorImpl()
+
+    @Test
+    fun `format check`() {
+        val ids = (1..10).map {
+            // given
+            val regex = Regex("""^QON_[a-zA-Z\d]{32}$""")
+
+            // when
+            val id = generator.generateUserId()
+
+            // then
+            assertThat(regex.matches(id)).isTrue
+            id
+        }
+        // check for duplicates
+        assertThat(ids.size).isEqualTo(ids.toSet().size)
+    }
+}
