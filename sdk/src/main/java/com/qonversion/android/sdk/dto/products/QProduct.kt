21120e8d5dd54e7962b37ec90c682e53c4a112c7
kamospertsyan <spertsiankamo@gmail.com>
2022-05-31 16:46:36 +0300

    Calculate product type and duration from SkuDetails.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
@@ -7,17 +7,27 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class QProduct(
     @Json(name = "id") val qonversionID: String,
-    @Json(name = "store_id") val storeID: String?,
-    @Json(name = "type") val type: QProductType,
-    @Json(name = "duration") val duration: QProductDuration?
+    @Json(name = "store_id") val storeID: String?
 ) {
     @Transient
     var skuDetail: SkuDetails? = null
         set(value) {
             prettyPrice = value?.price
-            trialDuration = configureTrialPeriod(value?.freeTrialPeriod)
+            duration = parseSubscriptionPeriod(value?.subscriptionPeriod)
+            trialDuration = parseTrialPeriod(value?.freeTrialPeriod)
+            type = when {
+                value?.type == "inapp" -> QProductType.InApp
+                trialDuration != QTrialDuration.NotAvailable -> QProductType.Trial
+                else -> QProductType.Subscription
+            }
             field = value
         }
+
+    @Transient
+    var type: QProductType = QProductType.Subscription
+
+    @Transient
+    var duration: QProductDuration? = null
 
     @Transient
     var offeringID: String? = null
@@ -28,25 +38,36 @@ data class QProduct(
     @Transient
     var trialDuration: QTrialDuration? = null
 
-    private fun configureTrialPeriod(trialPeriod: String?): QTrialDuration {
+    private fun parseTrialPeriod(trialPeriod: String?): QTrialDuration {
         if (trialPeriod.isNullOrEmpty()) {
             return QTrialDuration.NotAvailable
         }
 
-        var period: QTrialDuration = QTrialDuration.Other
-
-        when (trialPeriod) {
-            "P3D" -> period = QTrialDuration.ThreeDays
-            "P7D", "P1W" -> period = QTrialDuration.Week
-            "P14D", "P2W" -> period = QTrialDuration.TwoWeeks
-            "P30D", "P1M", "P4W2D" -> period = QTrialDuration.Month
-            "P60D", "P2M", "P8W4D" -> period = QTrialDuration.TwoMonths
-            "P90D", "P3M", "P12W6D" -> period = QTrialDuration.ThreeMonths
-            "P180D", "P6M", "P25W5D" -> period = QTrialDuration.SixMonths
-            "P365D", "P12M", "P52W1D" -> period = QTrialDuration.Year
+        return when (trialPeriod) {
+            "P3D" -> QTrialDuration.ThreeDays
+            "P7D", "P1W" -> QTrialDuration.Week
+            "P14D", "P2W" -> QTrialDuration.TwoWeeks
+            "P30D", "P1M", "P4W2D" -> QTrialDuration.Month
+            "P60D", "P2M", "P8W4D" -> QTrialDuration.TwoMonths
+            "P90D", "P3M", "P12W6D" -> QTrialDuration.ThreeMonths
+            "P180D", "P6M", "P25W5D" -> QTrialDuration.SixMonths
+            "P365D", "P1Y", "P12M", "P52W1D" -> QTrialDuration.Year
             else -> QTrialDuration.Other
         }
+    }
 
-        return period
+    private fun parseSubscriptionPeriod(subscriptionPeriod: String?): QProductDuration? {
+        if (subscriptionPeriod.isNullOrEmpty()) {
+            return null
+        }
+
+        return when (subscriptionPeriod) {
+            "P7D", "P1W" -> QProductDuration.Weekly
+            "P30D", "P1M", "P4W2D" -> QProductDuration.Monthly
+            "P90D", "P3M", "P12W6D" -> QProductDuration.ThreeMonthly
+            "P180D", "P6M", "P25W5D" -> QProductDuration.SixMonthly
+            "P365D", "P1Y", "P12M", "P52W1D" -> QProductDuration.Annual
+            else -> null
+        }
     }
 }
