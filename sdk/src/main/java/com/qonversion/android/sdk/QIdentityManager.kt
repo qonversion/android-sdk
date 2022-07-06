a1fc460a21536dfe4f65914f418ee819ac7817cb
Kamo Spertsyan <spertsiankamo@gmail.com>
2022-07-06 16:56:33 +0300

    Revert "Revert "Migrating identity API to v3 (#348)" (#367)"
    This reverts commit d376d893d6098f731238f7109edc0da610c95e7e.

--- sdk/src/main/java/com/qonversion/android/sdk/QIdentityManager.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/QIdentityManager.kt
@@ -1,11 +1,13 @@
 package com.qonversion.android.sdk
 
+import androidx.annotation.VisibleForTesting
 import com.qonversion.android.sdk.services.QUserInfoService
 import javax.inject.Inject
+import java.net.HttpURLConnection
 
 interface IdentityManagerCallback {
-    fun onSuccess(identityID: String)
-    fun onError(error: QonversionError)
+    fun onSuccess(qonversionUserId: String)
+    fun onError(error: QonversionError, responseCode: Int?)
 }
 
 class QIdentityManager @Inject constructor(
@@ -13,18 +15,45 @@ class QIdentityManager @Inject constructor(
     private val userInfoService: QUserInfoService
 ) {
     fun identify(userID: String, callback: IdentityManagerCallback) {
-        val currentUserID = userInfoService.obtainUserID()
-        repository.identify(userID, currentUserID,
-            onSuccess = { resultUserID ->
-                if (resultUserID.isNotEmpty()) {
-                    userInfoService.storeIdentity(resultUserID)
-                }
+        obtainIdentity(userID, object : IdentityManagerCallback {
+            override fun onSuccess(qonversionUserId: String) {
+                callback.onSuccess(qonversionUserId)
+            }
 
-                callback.onSuccess(resultUserID)
-            },
-            onError = {
-                callback.onError(it)
+            override fun onError(error: QonversionError, responseCode: Int?) {
+                if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+                    createIdentity(userID, callback)
+                } else {
+                    callback.onError(error, responseCode)
+                }
+            }
+        })
+    }
+
+    fun createIdentity(userID: String, callback: IdentityManagerCallback) {
+        val qonversionUserID = userInfoService.obtainUserID()
+        repository.createIdentity(qonversionUserID, userID,
+            onSuccess = { newQonversionUserId -> handleIdentity(callback, newQonversionUserId) },
+            onError = { error, code ->
+                callback.onError(error, code)
             })
+    }
+
+    fun obtainIdentity(userID: String, callback: IdentityManagerCallback) {
+        repository.obtainIdentity(userID,
+            onSuccess = { qonversionUserId -> handleIdentity(callback, qonversionUserId) },
+            onError = { error, code ->
+                callback.onError(error, code)
+            })
+    }
+
+    @VisibleForTesting
+    fun handleIdentity(callback: IdentityManagerCallback, resultUserID: String) {
+        if (resultUserID.isNotEmpty()) {
+            userInfoService.storeIdentity(resultUserID)
+        }
+
+        callback.onSuccess(resultUserID)
     }
 
     fun logoutIfNeeded(): Boolean {
