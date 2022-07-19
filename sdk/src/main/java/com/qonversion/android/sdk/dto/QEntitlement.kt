c17ee70dc91966f0fb247de0d5f385051bf1403c
kamospertsyan <spertsiankamo@gmail.com>
2022-07-19 17:53:51 +0300

    Immutable `active` flag.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/QEntitlement.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QEntitlement.kt
@@ -11,7 +11,7 @@ data class QEntitlement(
     @Json(name = "id") val permissionID: String,
     @Json(name = "started") val startedDate: Date,
     @Json(name = "expires") val expirationDate: Date?,
-    @Json(name = "active") internal var active: Boolean,
+    @Json(name = "active") internal val active: Boolean,
     @Json(name = "product") val product: Product
 ) {
     constructor(permission: QPermission) : this(
