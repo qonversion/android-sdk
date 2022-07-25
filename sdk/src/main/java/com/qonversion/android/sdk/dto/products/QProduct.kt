3dbbbe6bf31bf72a01c03d3f5884870890cfbd37
kamospertsyan <spertsiankamo@gmail.com>
2022-07-25 14:06:46 +0300

    Calculate entitlement duration depending on SkuDetails instead of QProduct.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/products/QProduct.kt
@@ -1,6 +1,7 @@
 package com.qonversion.android.sdk.dto.products
 
 import com.android.billingclient.api.SkuDetails
+import com.qonversion.android.sdk.converter.GoogleBillingPeriodConverter
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -16,7 +17,7 @@ data class QProduct(
     var skuDetail: SkuDetails? = null
         set(value) {
             prettyPrice = value?.price
-            trialDuration = configureTrialPeriod(value?.freeTrialPeriod)
+            trialDuration = GoogleBillingPeriodConverter.convertTrialPeriod(value?.freeTrialPeriod)
             field = value
         }
 
@@ -28,26 +29,4 @@ data class QProduct(
 
     @Transient
     var trialDuration: QTrialDuration? = null
-
-    private fun configureTrialPeriod(trialPeriod: String?): QTrialDuration {
-        if (trialPeriod.isNullOrEmpty()) {
-            return QTrialDuration.NotAvailable
-        }
-
-        var period: QTrialDuration = QTrialDuration.Other
-
-        when (trialPeriod) {
-            "P3D" -> period = QTrialDuration.ThreeDays
-            "P7D", "P1W" -> period = QTrialDuration.Week
-            "P14D", "P2W" -> period = QTrialDuration.TwoWeeks
-            "P30D", "P1M", "P4W2D" -> period = QTrialDuration.Month
-            "P60D", "P2M", "P8W4D" -> period = QTrialDuration.TwoMonths
-            "P90D", "P3M", "P12W6D" -> period = QTrialDuration.ThreeMonths
-            "P180D", "P6M", "P25W5D" -> period = QTrialDuration.SixMonths
-            "P365D", "P12M", "P52W1D" -> period = QTrialDuration.Year
-            else -> QTrialDuration.Other
-        }
-
-        return period
-    }
 }
