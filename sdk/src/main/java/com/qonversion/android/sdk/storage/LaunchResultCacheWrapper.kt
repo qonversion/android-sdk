3c59f19ed29d88aa3f45a02b7c5c8ba8237d6231
kamospertsyan <spertsiankamo@gmail.com>
2022-08-15 14:20:21 +0300

    Entitlements cache with fault tolerance

--- sdk/src/main/java/com/qonversion/android/sdk/storage/LaunchResultCacheWrapper.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/storage/LaunchResultCacheWrapper.kt
@@ -1,51 +1,73 @@
 package com.qonversion.android.sdk.storage
 
 import com.qonversion.android.sdk.billing.milliSecondsToSeconds
+import com.qonversion.android.sdk.daysToSeconds
 import com.qonversion.android.sdk.dto.QLaunchResult
-import com.qonversion.android.sdk.storage.LaunchResultCacheWrapper.CacheConstants.CACHE_TIMESTAMP_KEY
-import com.qonversion.android.sdk.storage.LaunchResultCacheWrapper.CacheConstants.DURATION_IN_HOURS_FOR_ACTUAL_CACHE
-import com.qonversion.android.sdk.storage.LaunchResultCacheWrapper.CacheConstants.LAUNCH_RESULT_KEY
+import com.qonversion.android.sdk.dto.QPermission
+import com.qonversion.android.sdk.dto.QPermissionsCacheLifetime
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import java.util.concurrent.TimeUnit
+import com.squareup.moshi.Types
+
+private const val LAUNCH_RESULT_KEY = "launchResult"
+private const val PERMISSIONS_KEY = "last_loaded_permissions"
+private const val CACHE_TIMESTAMP_KEY = "timestamp"
 
 class LaunchResultCacheWrapper(
     moshi: Moshi,
     private val cache: SharedPreferencesCache
 ) {
-    private val jsonAdapter: JsonAdapter<QLaunchResult> =
+    private val launchResultAdapter: JsonAdapter<QLaunchResult> =
         moshi.adapter(QLaunchResult::class.java)
 
-    fun getActualLaunchResult(): QLaunchResult? {
-        return if (isCacheOutdated()) {
+    private val permissionsAdapter: JsonAdapter<Map<String, QPermission>> =
+        moshi.adapter(
+            Types.newParameterizedType(
+                Map::class.java,
+                String::class.java,
+                QPermission::class.java
+            )
+        )
+
+    private var permissionsCacheLifetime = QPermissionsCacheLifetime.MONTH
+
+    fun setPermissionsCacheLifetime(lifetime: QPermissionsCacheLifetime) {
+        permissionsCacheLifetime = lifetime
+    }
+
+    fun getActualPermissions(): Map<String, QPermission>? {
+        return if (isCacheOutdated(permissionsCacheLifetime.days.daysToSeconds)) {
             null
         } else {
-            getLaunchResult()
+            getPermissions()
         }
     }
 
+    fun clearPermissionsCache() {
+        cache.remove(PERMISSIONS_KEY)
+    }
+
     fun getLaunchResult(): QLaunchResult? {
-        return cache.getObject(LAUNCH_RESULT_KEY, jsonAdapter)
+        return cache.getObject(LAUNCH_RESULT_KEY, launchResultAdapter)
+    }
+
+    private fun getPermissions(): Map<String, QPermission>? {
+        return cache.getObject(PERMISSIONS_KEY, permissionsAdapter)
     }
 
     fun save(launchResult: QLaunchResult) {
-        cache.putObject(LAUNCH_RESULT_KEY, launchResult, jsonAdapter)
+        cache.putObject(LAUNCH_RESULT_KEY, launchResult, launchResultAdapter)
+        cache.putObject(PERMISSIONS_KEY, launchResult.permissions, permissionsAdapter)
         val currentTime = getCurrentTimeInSec()
         cache.putLong(CACHE_TIMESTAMP_KEY, currentTime)
     }
 
-    private fun isCacheOutdated(): Boolean {
+    private fun isCacheOutdated(timeIsSec: Long): Boolean {
         val cachedTime = cache.getLong(CACHE_TIMESTAMP_KEY, 0)
         val currentTime = getCurrentTimeInSec()
 
-        return currentTime - cachedTime >= TimeUnit.HOURS.toSeconds(DURATION_IN_HOURS_FOR_ACTUAL_CACHE)
+        return currentTime - cachedTime >= timeIsSec
     }
 
     private fun getCurrentTimeInSec() = System.currentTimeMillis().milliSecondsToSeconds()
-
-    private object CacheConstants {
-        const val LAUNCH_RESULT_KEY = "launchResult"
-        const val CACHE_TIMESTAMP_KEY = "timestamp"
-        const val DURATION_IN_HOURS_FOR_ACTUAL_CACHE = 24L
-    }
 }
