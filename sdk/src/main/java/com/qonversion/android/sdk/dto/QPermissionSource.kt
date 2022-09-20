beda188c695fcb8df7c1406abac6815e7a2924ed
kamospertsyan <spertsiankamo@gmail.com>
2022-09-20 12:39:23 +0300

    Detekt fixes.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/QPermissionSource.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QPermissionSource.kt
@@ -1,11 +1,11 @@
 package com.qonversion.android.sdk.dto
 
 enum class QPermissionSource(internal val key: String) {
-    Unknown("unknown"),     // Unable to detect the source
-    AppStore("appstore"),   // App Store
+    Unknown("unknown"), // Unable to detect the source
+    AppStore("appstore"), // App Store
     PlayStore("playstore"), // Play Store
-    Stripe("stripe"),       // Stripe
-    Manual("manual");       // The entitlement was activated manually
+    Stripe("stripe"), // Stripe
+    Manual("manual"); // The entitlement was activated manually
 
     companion object {
         fun fromKey(key: String): QPermissionSource {
