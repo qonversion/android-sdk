175437d83fe2b2f3e54bdf7de272d699ffd5db3e
kamospertsyan <spertsiankamo@gmail.com>
2022-10-28 11:14:41 +0600

    CR fixes

--- app/src/main/java/com/qonversion/android/app/EntitlementsFragment.kt
+++ app/src/main/java/com/qonversion/android/app/EntitlementsFragment.kt
@@ -9,25 +9,25 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.qonversion.android.app.databinding.FragmentPermissionsBinding
+import com.qonversion.android.app.databinding.FragmentEntitlementsBinding
 import com.qonversion.android.sdk.Qonversion
 import com.qonversion.android.sdk.dto.QEntitlement
 import com.qonversion.android.sdk.dto.QonversionError
 import com.qonversion.android.sdk.listeners.QonversionEntitlementsCallback
 
-class PermissionsFragment : Fragment() {
-    private val TAG = "PermissionsFragment"
-    lateinit var binding: FragmentPermissionsBinding
+class EntitlementsFragment : Fragment() {
+    private val TAG = "EntitlementsFragment"
+    lateinit var binding: FragmentEntitlementsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentPermissionsBinding.inflate(inflater)
+        binding = FragmentEntitlementsBinding.inflate(inflater)
 
-        binding.recyclerViewPermissionsList.layoutManager = LinearLayoutManager(context)
-        binding.recyclerViewPermissionsList.addItemDecoration(
+        binding.recyclerViewEntitlementsList.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewEntitlementsList.addItemDecoration(
             DividerItemDecoration(
                 context,
                 DividerItemDecoration.VERTICAL
@@ -36,8 +36,8 @@ class PermissionsFragment : Fragment() {
 
         Qonversion.sharedInstance.checkEntitlements(object : QonversionEntitlementsCallback {
             override fun onSuccess(entitlements: Map<String, QEntitlement>) {
-                val activePermissions = entitlements.values.filter { it.isActive }
-                binding.recyclerViewPermissionsList.adapter = PermissionsAdapter(activePermissions)
+                val activeEntitlements = entitlements.values.filter { it.isActive }
+                binding.recyclerViewEntitlementsList.adapter = EntitlementsAdapter(activeEntitlements)
             }
 
             override fun onError(error: QonversionError) {
--- sdk/src/main/java/com/qonversion/android/sdk/dto/QEntitlement.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QEntitlement.kt
@@ -1,15 +1,13 @@
 package com.qonversion.android.sdk.dto
 
 import com.qonversion.android.sdk.internal.dto.QPermission
-import com.qonversion.android.sdk.internal.dto.QProductRenewState
-import com.qonversion.android.sdk.internal.toInt
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import java.util.Date
 
 @JsonClass(generateAdapter = true)
 data class QEntitlement(
-    @Json(name = "id") val permissionID: String,
+    @Json(name = "id") val id: String,
     @Json(name = "started") val startedDate: Date,
     @Json(name = "expires") val expirationDate: Date?,
     @Json(name = "active") internal val active: Boolean,
@@ -32,19 +30,9 @@ data class QEntitlement(
 
     val isActive get() = active
 
-    internal fun toPermission(): QPermission = QPermission(
-        permissionID,
-        product.productID,
-        product.subscription?.renewState?.toProductRenewState() ?: QProductRenewState.Unknown,
-        startedDate,
-        expirationDate,
-        source,
-        active.toInt()
-    )
-
     @JsonClass(generateAdapter = true)
     data class Product(
-        @Json(name = "product_id") val productID: String,
+        @Json(name = "product_id") val productId: String,
         @Json(name = "subscription") val subscription: Subscription?
     ) {
         @JsonClass(generateAdapter = true)
