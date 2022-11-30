b085b9387fc1cbbb702fe058d7d2337d5a9ba42e
kamospertsyan <spertsiankamo@gmail.com>
2022-11-30 18:44:57 +0600

    QTrialDuration.Unknown added for the default value, while we don't have SkuDetails.

--- sdk/src/main/java/com/qonversion/android/sdk/internal/converter/GoogleBillingPeriodConverter.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/converter/GoogleBillingPeriodConverter.kt
@@ -16,17 +16,15 @@ internal object GoogleBillingPeriodConverter {
             return QTrialDuration.NotAvailable
         }
 
-        var period: QTrialDuration = QTrialDuration.Other
-
-        when (trialPeriod) {
-            "P3D" -> period = QTrialDuration.ThreeDays
-            "P7D", "P1W" -> period = QTrialDuration.Week
-            "P14D", "P2W" -> period = QTrialDuration.TwoWeeks
-            "P30D", "P1M", "P4W2D" -> period = QTrialDuration.Month
-            "P60D", "P2M", "P8W4D" -> period = QTrialDuration.TwoMonths
-            "P90D", "P3M", "P12W6D" -> period = QTrialDuration.ThreeMonths
-            "P180D", "P6M", "P25W5D" -> period = QTrialDuration.SixMonths
-            "P365D", "P12M", "P52W1D", "P1Y" -> period = QTrialDuration.Year
+        val period = when (trialPeriod) {
+            "P3D" -> QTrialDuration.ThreeDays
+            "P7D", "P1W" -> QTrialDuration.Week
+            "P14D", "P2W" -> QTrialDuration.TwoWeeks
+            "P30D", "P1M", "P4W2D" -> QTrialDuration.Month
+            "P60D", "P2M", "P8W4D" -> QTrialDuration.TwoMonths
+            "P90D", "P3M", "P12W6D" -> QTrialDuration.ThreeMonths
+            "P180D", "P6M", "P25W5D" -> QTrialDuration.SixMonths
+            "P365D", "P12M", "P52W1D", "P1Y" -> QTrialDuration.Year
             else -> QTrialDuration.Other
         }
 
