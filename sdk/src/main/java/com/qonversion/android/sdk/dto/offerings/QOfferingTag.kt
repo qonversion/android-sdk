9dfed495a573a0f7d55437d1c66140a25568adc8
kamospertsyan <spertsiankamo@gmail.com>
2022-11-30 18:44:24 +0600

    QOfferingTag.Unknown added for the cases, when we can't parse value.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/offerings/QOfferingTag.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/offerings/QOfferingTag.kt
@@ -1,14 +1,16 @@
 package com.qonversion.android.sdk.dto.offerings
 
 enum class QOfferingTag(val tag: Int?) {
+    Unknown(-1),
     None(0),
     Main(1);
 
     companion object {
         fun fromTag(tag: Int?): QOfferingTag {
             return when (tag ?: 0) {
+                0 -> None
                 1 -> Main
-                else -> None
+                else -> Unknown
             }
         }
     }
