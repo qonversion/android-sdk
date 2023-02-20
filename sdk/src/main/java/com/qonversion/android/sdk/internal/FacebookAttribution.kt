6d5fe065d57b5e1e81941605e3ac87d42d85b196
GitHub Action <action@github.com>
2023-02-20 16:52:06 +0300

    Facebook attribution request made async to prevent from ANRs

--- sdk/src/main/java/com/qonversion/android/sdk/internal/FacebookAttribution.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/internal/FacebookAttribution.kt
@@ -2,11 +2,23 @@ package com.qonversion.android.sdk.internal
 
 import android.content.ContentResolver
 import android.net.Uri
+import java.lang.ref.WeakReference
+import java.util.concurrent.Executors
+
+interface FacebookAttributionListener {
+  fun onFbAttributionIdResult(id: String?)
+}
 
 internal class FacebookAttribution {
-    fun getAttributionId(contentResolver: ContentResolver): String? {
-        val projection =
-            arrayOf(ATTRIBUTION_ID_COLUMN_NAME)
+    fun getAttributionId(contentResolver: ContentResolver, resultListener: FacebookAttributionListener) {
+        val listenerRef = WeakReference(resultListener)
+        Executors.newSingleThreadExecutor().execute {
+            getAttributionIdAsync(contentResolver, listenerRef)
+        }
+    }
+
+    private fun getAttributionIdAsync(contentResolver: ContentResolver, listenerRef: WeakReference<FacebookAttributionListener>) {
+        val projection = arrayOf(ATTRIBUTION_ID_COLUMN_NAME)
         val content = try {
             contentResolver.query(
                 ATTRIBUTION_ID_CONTENT_URI,
@@ -18,17 +30,18 @@ internal class FacebookAttribution {
         } catch (e: Exception) {
             null
         }
-        if (content == null || !content.moveToFirst()) {
-            return null
-        }
-        val columnIndex = content.getColumnIndex(ATTRIBUTION_ID_COLUMN_NAME)
-        if (columnIndex < 0) {
-            return null
-        }
-        val attributionId = content.getString(columnIndex)
-        content.close()
 
-        return attributionId
+        var attributionId: String? = null
+
+        if (content?.moveToFirst() == true) {
+            val columnIndex = content.getColumnIndex(ATTRIBUTION_ID_COLUMN_NAME)
+            if (columnIndex >= 0) {
+                attributionId = content.getString(columnIndex)
+                content.close()
+            }
+        }
+
+        listenerRef.get()?.onFbAttributionIdResult(attributionId)
     }
 
     companion object {
