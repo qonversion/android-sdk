88fca44623ab5c091331529f7ba3defb931277c6
Kamo Spertsyan <spertsiankamo@gmail.com>
2023-11-24 10:42:19 +0300

    Added comments to purchase models.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseModel.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseModel.kt
@@ -1,5 +1,18 @@
 package com.qonversion.android.sdk.dto
 
+import com.qonversion.android.sdk.Qonversion
+import com.qonversion.android.sdk.dto.products.QProduct
+
+/**
+ * Used to provide all the necessary purchase data to the [Qonversion.purchase] method.
+ * Can be created manually or using the [QProduct.toPurchaseModel] method.
+ *
+ * If [offerId] is not specified, then the default offer will be applied - we will choose
+ * the cheapest offer for the client.
+ *
+ * To prevent applying any offer to the purchase (use only bare base plan),
+ * call the [QPurchaseModel.removeOffer] method.
+ */
 data class QPurchaseModel(
     val qonversionProductId: String,
     var offerId: String? = null
--- sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseUpdateModel.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseUpdateModel.kt
@@ -1,5 +1,21 @@
 package com.qonversion.android.sdk.dto
 
+import com.qonversion.android.sdk.Qonversion
+import com.qonversion.android.sdk.dto.products.QProduct
+
+/**
+ * Used to provide all the necessary purchase data to the [Qonversion.updatePurchase] method.
+ * Can be created manually or using the [QProduct.toPurchaseUpdateModel] method.
+ *
+ * If [offerId] is not specified, then the default offer will be applied - we will choose
+ * the cheapest offer for the client.
+ *
+ * To prevent applying any offer to the purchase (use only bare base plan),
+ * call the [QPurchaseModel.removeOffer] method.
+ *
+ * If the [updatePolicy] is not provided, then default one
+ * will be selected - [QPurchaseUpdatePolicy.WithTimeProration].
+ */
 data class QPurchaseUpdateModel(
     val qonversionProductId: String,
     var oldQonversionProductId: String,
