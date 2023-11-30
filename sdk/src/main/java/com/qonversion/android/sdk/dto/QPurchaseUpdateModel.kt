5f29e3d490169ab53e5717661b9e04ed879e4e81
Kamo Spertsyan <spertsiankamo@gmail.com>
2023-11-30 10:16:35 +0300

    Comment fix.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseUpdateModel.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/QPurchaseUpdateModel.kt
@@ -8,12 +8,11 @@ import com.qonversion.android.sdk.dto.products.QProductStoreDetails
  * Used to provide all the necessary purchase data to the [Qonversion.updatePurchase] method.
  * Can be created manually or using the [QProduct.toPurchaseUpdateModel] method.
  *
- * If [offerId] is not specified, then the default offer will be applied - we will choose
- * the cheapest offer for the client.
+ * If [offerId] is not specified, then the default offer will be applied. To know how we choose
+ * the default offer, see [QProductStoreDetails.defaultSubscriptionOfferDetails].
  *
  * To prevent applying any offer to the purchase (use only bare base plan),
  * call the [QPurchaseModel.removeOffer] method.
- * To know how we choose the default offer, see [QProductStoreDetails.defaultSubscriptionOfferDetails].
  *
  * If the [updatePolicy] is not provided, then default one
  * will be selected - [QPurchaseUpdatePolicy.WithTimeProration].
