b01902b3aa28e1fc05917ecb66040289a510a74c
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-01-25 17:57:20 +0300

    Added properties for PushWoosh and AppMetrica integration

--- sdk/src/main/java/com/qonversion/android/sdk/dto/properties/QUserPropertyKey.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/properties/QUserPropertyKey.kt
@@ -11,6 +11,10 @@ enum class QUserPropertyKey(val userPropertyCode: String) {
     FirebaseAppInstanceId("_q_firebase_instance_id"),
     AppSetId("_q_app_set_id"),
     AdvertisingId("_q_advertising_id"), // iOS only
+    PushWooshUserId("_q_push_woosh_user_id"),
+    PushWooshHwId("_q_push_woosh_hwid"),
+    AppMetricaDeviceId("_q_app_metrica_device_id"),
+    AppMetricaUserProfileId("_q_app_metrica_user_profile_id"),
     Custom("");
 
     companion object {
