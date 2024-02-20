aa5316b079c7cca4cb516cbdae06ab89bec8509e
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-02-20 11:22:03 +0300

    AppMetricaDeviceId property renamed to AppMetricaDeviceIdHash.

--- sdk/src/main/java/com/qonversion/android/sdk/dto/properties/QUserPropertyKey.kt
+++ sdk/src/main/java/com/qonversion/android/sdk/dto/properties/QUserPropertyKey.kt
@@ -11,7 +11,7 @@ enum class QUserPropertyKey(val userPropertyCode: String) {
     FirebaseAppInstanceId("_q_firebase_instance_id"),
     AppSetId("_q_app_set_id"),
     AdvertisingId("_q_advertising_id"), // iOS only
-    AppMetricaDeviceId("_q_appmetrica_device_id"),
+    AppMetricaDeviceIdHash("_q_appmetrica_device_id"),
     AppMetricaUserProfileId("_q_appmetrica_user_profile_id"),
     PushWooshHwId("_q_pushwoosh_hwid"),
     PushWooshUserId("_q_pushwoosh_user_id"),
