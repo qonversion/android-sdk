e7866508940eb689f98f320aa333502b68ee6414
Kamo Spertsyan <spertsiankamo@gmail.com>
2025-02-04 16:53:39 +0300

    Sample app migration, initial.

---
+++ sample/src/main/java/io/qonversion/sample/MainActivity.kt
@@ -0,0 +1,34 @@
+package io.qonversion.sample
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import io.qonversion.sample.R
+import io.qonversion.sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    companion object {
+        fun getCallingIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+}
---
+++ sample/src/main/java/io/qonversion/sample/OfferingsFragment.kt
@@ -0,0 +1,72 @@
+package io.qonversion.sample
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.qonversion.android.sdk.Qonversion
+import com.qonversion.android.sdk.dto.QonversionError
+import com.qonversion.android.sdk.dto.entitlements.QEntitlement
+import com.qonversion.android.sdk.dto.offerings.QOfferings
+import com.qonversion.android.sdk.dto.products.QProduct
+import com.qonversion.android.sdk.listeners.QonversionEntitlementsCallback
+import com.qonversion.android.sdk.listeners.QonversionOfferingsCallback
+import io.qonversion.sample.databinding.FragmentOfferingsBinding
+
+private const val TAG = "OfferingsFragment"
+
+class OfferingsFragment : Fragment() {
+    lateinit var binding: FragmentOfferingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOfferingsBinding.inflate(inflater)
+
+        binding.recyclerViewProductsList.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewProductsList.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        Qonversion.shared.offerings(object : QonversionOfferingsCallback {
+            override fun onSuccess(offerings: QOfferings) {
+                val mainProducts = offerings.main?.products
+                mainProducts?.let {
+                    binding.recyclerViewProductsList.adapter = ProductsAdapter(it) { product ->
+                        purchase(product)
+                    }
+                } ?:  Toast.makeText(context, "There in no product in the main offering", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: QonversionError) {
+                Toast.makeText(context, error.description, Toast.LENGTH_LONG).show()
+                Log.e(TAG, error.toString())
+            }
+        })
+
+        return binding.root
+    }
+
+    private fun purchase(product: QProduct) {
+        Qonversion.shared.purchase(requireActivity(), product.toPurchaseModel(), callback = object :
+            QonversionEntitlementsCallback {
+            override fun onSuccess(entitlements: Map<String, QEntitlement>) {
+                Toast.makeText(context, "Purchase succeeded", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: QonversionError) {
+                showError(requireContext(), error, TAG)
+            }
+        })
+    }
+}
