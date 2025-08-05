91917710796e3cb3ff3481f972579fcd7bb89658
Surik <action@github.com>
2025-08-05 17:43:48 +0300

    Removed unused import

--- nocodes/src/main/java/io/qonversion/nocodes/internal/screen/service/FallbackService.kt
+++ nocodes/src/main/java/io/qonversion/nocodes/internal/screen/service/FallbackService.kt
@@ -1,6 +1,5 @@
 package io.qonversion.nocodes.internal.screen.service
 
-import android.content.Context
 import io.qonversion.nocodes.internal.dto.NoCodeScreen
 
 internal interface FallbackService {
