56cd1944561656db72da16267850ea97464fd8d1
Surik <action@github.com>
2025-08-05 17:40:45 +0300

    Removed unused func

--- nocodes/src/main/java/io/qonversion/nocodes/internal/screen/service/FallbackService.kt
+++ nocodes/src/main/java/io/qonversion/nocodes/internal/screen/service/FallbackService.kt
@@ -6,14 +6,4 @@ import io.qonversion.nocodes.internal.dto.NoCodeScreen
 internal interface FallbackService {
     suspend fun loadScreen(contextKey: String): NoCodeScreen?
     suspend fun loadScreenById(screenId: String): NoCodeScreen?
-
-    companion object {
-        fun isFallbackFileAvailable(fileName: String, context: Context): Boolean {
-            return try {
-                context.assets.open(fileName).use { true }
-            } catch (e: Exception) {
-                false
-            }
-        }
-    }
 }
